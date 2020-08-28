2d670fbd51a655d7090bc2704041155e5037f7da
Abrham <abrhamasfaw14@gmail.com>
2020-08-28 14:05:03 -0400

    Updates

---
+++ precache-manifest.aba7a3b5bfb0a5186b1421cdd541b020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78852a0a238f0947f6a82e512c0d44d0",
+    "url": "/abrhamasfaw14/searched--country.git/index.html"
+  },
+  {
+    "revision": "41c5a8cefeb5b04789bd",
+    "url": "/abrhamasfaw14/searched--country.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13cd460a2fdcf833c60f",
+    "url": "/abrhamasfaw14/searched--country.git/static/js/2.f70943ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abrhamasfaw14/searched--country.git/static/js/2.f70943ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c5a8cefeb5b04789bd",
+    "url": "/abrhamasfaw14/searched--country.git/static/js/main.c5942c6e.chunk.js"
+  },
+  {
+    "revision": "6e8046db060ccce9e26d",
+    "url": "/abrhamasfaw14/searched--country.git/static/js/runtime-main.cdd52a5f.js"
+  }
+]);
